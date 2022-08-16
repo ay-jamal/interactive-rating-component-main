218ae84d6d2fdd4197edb6073d120385ef54a440
Ayhem jamal <76484434+ay-jamal@users.noreply.github.com>
2022-08-16 14:55:41 +0200

    upload to github

---
+++ app.js
@@ -0,0 +1,38 @@
+let button = document.querySelectorAll(".btn");
+let container = document.querySelector(".container");
+let submitBtn = document.getElementById("btn");
+
+// button.addEventListener("click", () => {
+//   console.log("ayhem");
+// });
+
+let count = 0;
+
+button.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    count++;
+    btn.classList.toggle("active");
+    console.log(count);
+  });
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (count > 0) {
+    container.innerHTML = `
+    <div class="container card">
+    <img src="/images/illustration-thank-you.svg" alt="" />
+    <span>You Selected ${count} out of 5</span>
+
+    <div class="text">
+      <p>Thank you!</p>
+
+      We appreciate you taking the time to give a rating. If you ever need
+      more support, don’t hesitate to get in touch!
+    </div>
+  </div>
+    
+  
+    `;
+  }
+});
